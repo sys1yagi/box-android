4f64af2ca7ec2202d340e8592dc057b0db0c2cea
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2015-05-01 19:08:28 +0900

    Optimize settings.

--- install-build-tools.sh
+++ install-build-tools.sh
@@ -2,5 +2,6 @@
 
 wget https://dl-ssl.google.com/android/repository/build-tools_r${ANDROID_BUILD_TOOLS}-linux.zip
 unzip build-tools_r${ANDROID_BUILD_TOOLS}-linux.zip -d $ANDROID_HOME
+rm -fr build-tools_r${ANDROID_BUILD_TOOLS}-linux.zip
 mkdir -p $ANDROID_HOME/build-tools/
 mv $ANDROID_HOME/android-${ANDROID_VERSION} $ANDROID_HOME/build-tools/${ANDROID_BUILD_TOOLS}
--- install-gradle.sh
+++
@@ -1,6 +0,0 @@
-cd /tmp
-wget http://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip
-unzip gradle-$GRADLE_VERSION-bin.zip
-mv gradle-$GRADLE_VERSION $HOME
-
-
